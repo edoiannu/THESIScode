9a8205bbffb95ec1202a9ecde2ede98c54bfcd7c
edoiannu <edoiannuc@gmail.com>
2026-06-21 16:34:19 +0200

    update

--- code/data_analysis.py
+++ code/data_analysis.py
@@ -1,41 +1,24 @@
-# === DAEMONIC ERGOTROPY AND CAPACITY (AND RESPECTIVE MOMENTA) COMPUTATION ===
+# === DAEMONIC ERGOTROPY AND CAPACITY (AND RESPECTIVE MOMENTA) PLOT GENERATION FOR CONTINUOUSLY MONITORED OPEN QUANTUM SYSTEMS ===
 
-# import required libraries and objects
-import pickle
-import sys
+# importing required libraries
+import os
 import numpy as np
-import os                                           # to count the cores
-from concurrent.futures import ProcessPoolExecutor  # for parallel computing  
-import time                                         # to count the execution time of a process
-from my_library.my_system import *                  # we import everything from my_library
-from my_library.my_functions import *
-from qutip import qeye
-import pickle   # to print complex objects on a file as they are
-import gc       # garbage collector for manual memory cleanup
-
-# command that gives the number of usable physical cores (for an eventually parallel compututing)
-num_cores = int(os.environ.get("SLURM_CPUS_PER_TASK", os.cpu_count()))
+import matplotlib.pyplot as plt
 
 # variables initialization
-unravelling = None              # type of unravveling {pd (photodection), phi0 (homodyne detection with phi = 0°), phi90 (homodyne detection with phi = 90°) or hd (heterodyne detection)}
-instate = None                  # initial state as a single character variable describing the evolution initial state {p (pure), m (maximally mixed)}
-phivalue = None                 # phi value for a homodyne detection               
-alpha_over_kappa = None         # driving field intensity over the system emission rate
-eta = None                      # detection efficiency
-chunk_dim = None                # chunk dimension (for parallel computing)
-NUMBER_OF_TRAJECTORIES = None   # number of evolved trajectories
-NUMBER_OF_TIMEINTERVALS = None  # number of time intervals per trajectory
+alpha_over_kappa = None
+eta = None
+instate = None
+t_f = None
+dt = None
+NUMBER_OF_TRAJECTORIES = None
 
-# we pass the unravelling as arguments of command line
-unravelling = sys.argv[1]
-
-# reading from file data analysis parameters
-with open("data_analysis.dat", "r") as f:
+with open("input.dat", "r") as f:
     # cycle on every line of the file
     for line in f:
         # words separated by a space within a line become the elements of a list
         parts = line.split()
-        # it skips empty lines, controls that there are exactly two elements per line (otherwise it skips the line) and skips the comments
+        # it skips empty lines, controls that there are exactly two elements per line (otherwise it skips the line) and skip the comments
         if not line or len(parts) != 2 or line.startswith("#"):
             continue
         key, value = parts
@@ -45,104 +28,185 @@ with open("data_analysis.dat", "r") as f:
             alpha_over_kappa = float(value)
         elif key == "ETA":
             eta = float(value)
-        elif key == "CHUNKDIM":
-            chunk_dim = int(value)
-
-if unravelling in {"pd", "hod0", "hod90", "hed"}:
-    process = unravelling + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa)    # process name
-else:
-    raise ValueError("The unravelling must be photodection, homodyne detection with phi = {0°, 90°} or heterodyne detection {pd, hod0, hod90, hed}")
-
-# initial state as density matrix
-if instate == "p":
-    rho0 = ebasis[0] * ebasis[0].dag()  # ground state
-elif instate == "m":
-    rho0 = 0.5 * qeye(DIMH)             # maximally mixed state (one half the identity matrix)
-else:
-    raise ValueError("The initial state must be pure (p) or maximally mixed (m).")
-
-# directory storing data and results
-if eta == 0.4 and alpha_over_kappa == 1.0:
-    directory = "figure1/"
-elif eta == 1.0 and alpha_over_kappa == 0.4:
-    directory = "figure2/"
-else:
-    directory = "./"
-
-# reading the number of trajectories and time intervals
-with open(directory + "/input.dat", "r") as f:
-    for line in f:
-        parts = line.split()
-        if not line or len(parts) != 2 or line.startswith("#"):
-            continue
-        key, value = parts
-        if key == "NTRAJ":
-            NUMBER_OF_TRAJECTORIES = int(value)
         elif key == "FINALT":
             t_f = float(value)
         elif key == "dt":
             dt = float(value)
+        elif key == "NTRAJ":
+            NUMBER_OF_TRAJECTORIES = int(value)
 
-chunk_num = int(NUMBER_OF_TRAJECTORIES / chunk_dim)     # number of chunk
-NUMBER_OF_TIMEINTERVALS = int(t_f / dt)                 # number of time intervals
-tlist = np.linspace(0, t_f, NUMBER_OF_TIMEINTERVALS)    # list of time intervals
+NUMBER_OF_TIMEINTERVALS = int(t_f / dt) # number of time intervals
+resultsdir = "results/"                 # results directory
+plotsdir = "plots/"                     # output directory for saved figures
+os.makedirs(plotsdir, exist_ok = True)
 
-start = time.time()
+# labels
+unr = ["pd", "hod0", "hod90", "hed"]                                                                    # unravellings
+unr_colors = ["red", "blue", "orange", "green"]                                                         # colors for the unravellings
+obs = ["erg", "cap"]                                                                                    # observables
+obs_labels = [r"\bar{\epsilon}", r"\bar{\mathcal{C}}"]                                                  # observable labels
+unr_labels = ["PD", r"HoD \left< \hat{\sigma_x} \right>", r"HoD \left< \hat{\sigma_y} \right>", "HeD"]  # unravelling labels
 
-prog_erg_sum = None # list of the progressive sum and squared sum of the daemonic ergotropies
-prog_cap_sum = None # list of the progressive sum and squadre sum of the daemonic capacities
+# === EXPECTATION VALUEs ===
 
-erg_mean = []       # list to fill with the averaged over the trajectories ergotropies at each time
-cap_mean = []       # list to fill with the averaged over the trajectories capacities at each time
-erg_var = []        # list to fill with the variance of the ergotropies at each time
-cap_var = []        # list to fill with the variance of the capacities at each time
-erg_skw = []        # list to fill with the skewness of the capacities at each time
-cap_skw = []        # list to fill with the skewness of the capacities at each time
+# prefixes and labels of unconditional dynamics
+unc_prefixes = {"erg": "erg_unc_", "cap": "cap_unc_"}
+unc_labels = {"erg": r"$\epsilon_{unc}(t)$", "cap": r"$\mathcal{C}_{unc}(t)$"}
 
-print("Averaged quantities computation (unravelling: ", unravelling, ", initial ", instate, " state, alpha/kappa = ", alpha_over_kappa, ", eta = ", eta, ", ", NUMBER_OF_TIMEINTERVALS, " time intervals and ", NUMBER_OF_TRAJECTORIES, " trajectories)...", sep = "")
-int_time = 0    # progressive run time
+# loop over physical quantities (ergotropy and capacity)
+for i in range(len(obs)):
+    # plot initialization
+    fig, ax = plt.subplots(figsize=(6, 5))
+    # loop over the unravellings
+    for j in range(len(unr)):
+        data = []
+        data = np.loadtxt(resultsdir + obs[i] + "_" + unr[j] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".dat", delimiter="\t")
+        ax.plot(data[:,0], data[:,1], color = unr_colors[j], label = r"$" + obs_labels[i] + r"_{" + unr_labels[j] + ", " + str(eta) + r"}$")
+    # reading energy evolution from file
+    if obs[i] == "erg":
+        data = []
+        data = np.loadtxt(resultsdir + "en_unc_" + instate + "_alpha" + str(alpha_over_kappa) + ".dat")
+        ax.plot(data[:,0], data[:,1], color = "gray", label = r"$E_{unc}(t)$")
+    # reading unconditional quantities evolution
+    data = []
+    data = np.loadtxt(resultsdir + unc_prefixes[obs[i]] + instate + "_alpha" + str(alpha_over_kappa) + ".dat")
+    ax.plot(data[:,0], data[:,1], color = "black", label = unc_labels[obs[i]])
 
-with ProcessPoolExecutor(max_workers = num_cores) as executor:
-    for i in range(chunk_num):
-        chunk_start_time = time.time()  # we start counting the execution time of the chunk
-       # we open the file in binary reading mode
-        with open("states/" + process +  "_chunk" + str(i+1) + ".pkl", "rb") as f:
-            states = np.array(pickle.load(f), dtype = object)     # we load the i-th chunk, conversion useful to use the function 'executor.map' on the list of states at each time interval
-        # rho = np.array(states, dtype = object)
-        rho = [states[0 : chunk_dim, j] for j in range(NUMBER_OF_TIMEINTERVALS)] # list of lists of the different trajectories states at given times
-        # we map the cores on the time intervals to compute the averaged daemonic ergotropy
-        erg_chunk = np.array(list(executor.map(av_ergotropy, rho)))
-        cap_chunk = np.array(list(executor.map(av_capacity, rho)))
-        del states, rho     # remove reference to solver result to free memory
-        gc.collect()        # force garbage collection to release unused memory
-        if prog_erg_sum is None:
-            prog_erg_sum = erg_chunk
-        else:
-            prog_erg_sum += erg_chunk
-        if prog_cap_sum is None:
-            prog_cap_sum = cap_chunk
-        else:
-            prog_cap_sum += cap_chunk
-        chunk_end_time = time.time()    # we end counting the execution time of the chunk
-        int_time += chunk_end_time - chunk_start_time
-        print(round(int((i + 1) * chunk_dim) / NUMBER_OF_TRAJECTORIES * 100, 1), "%. Run time: ", round(int_time, 2), "s.", sep = "")
-erg_mean = prog_erg_sum[:, 0] / chunk_num
-cap_mean = prog_cap_sum[:, 0] / chunk_num
-erg_var = prog_erg_sum[:, 1] / chunk_num - erg_mean ** 2
-cap_var = prog_cap_sum[:, 1] / chunk_num - cap_mean ** 2
-# erg_skw = [(prog_erg_sum[:, 2] / chunk_num - 3 * erg_mean * erg_var - erg_mean ** 3) / np.sqrt(i) ** 3 for i in erg_var]
-# cap_skw = [(prog_cap_sum[:, 2] / chunk_num - 3 * cap_mean * cap_var - cap_mean ** 3) / np.sqrt(i) ** 3 for i in cap_var]
+    # costumize axes and grids
+    ax.set_xlabel(r"$\kappa t$")
+    ax.set_ylabel(r"$" + obs_labels[i] + r"_{unr, \eta} (t) / \omega_0$")
+    ax.grid(True, linestyle = ':', alpha = 0.6)
 
-end = time.time()
+    # costumize legend
+    ax.legend(loc = "upper right", ncol = 2)
 
-# total execution time
-print("Total run time: ", round(end - start, 2), "s")
+    fig.suptitle(
+            f"Mean values ({obs[i]}): initial pure state, "  r"$\alpha / \kappa$" f" = {alpha_over_kappa},\n" f"{NUMBER_OF_TIMEINTERVALS : .1e} time intervals and{NUMBER_OF_TRAJECTORIES : .1e} trajectories."
+        )
 
-# we print the results on a file
-print("Printing results...")
-np.savetxt(directory + "results/erg_" + process + ".dat", np.column_stack((tlist, erg_mean)), delimiter="\t", fmt=["%.3f", "%.8f"])
-np.savetxt(directory + "results/cap_" + process + ".dat", np.column_stack((tlist, cap_mean)), delimiter="\t", fmt=["%.3f", "%.8f"])
-np.savetxt(directory + "results/var_erg_" + process + ".dat", np.column_stack((tlist, erg_var)), delimiter="\t", fmt=["%.3f", "%.8f"])
-np.savetxt(directory + "results/var_cap_" + process + ".dat", np.column_stack((tlist, cap_var)), delimiter="\t", fmt=["%.3f", "%.8f"])
-# np.savetxt(directory + "results/skw_erg_" + process + ".dat", np.column_stack((tlist, erg_skw)), delimiter="\t", fmt=["%.3f", "%.8f"])
-# np.savetxt(directory + "results/skw_cap_" + process + ".dat", np.column_stack((tlist, cap_skw)), delimiter="\t", fmt=["%.3f", "%.8f"])
+    # layout optimization and saving
+    plt.tight_layout()
+    plt.savefig(plotsdir + "mean_" + obs[i] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".png", dpi = 300)
+    plt.close(fig)
+
+# === VARIANCES ===
+
+for i in range(len(obs)):
+    fig, ax = plt.subplots(figsize=(6, 5))
+    # loop over the unravellings
+    for j in range(len(unr)):
+        data = []
+        data = np.loadtxt(resultsdir + "var_" + obs[i] + "_" + unr[j] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".dat", delimiter="\t")
+        ax.plot(data[:,0], data[:,1], color = unr_colors[j], label = r"$" + obs_labels[i] + r"_{" + unr_labels[j] + ", " + str(eta) + r"}$")
+
+    # costumize axes and grids
+    ax.set_xlabel(r"$\kappa t$")
+    ax.set_ylabel(r"$\sigma^2_" + obs_labels[i] + " (t)$")
+    ax.grid(True, linestyle = ':', alpha = 0.6)
+
+    ax.legend(loc = "upper right", ncol = 2)
+
+    fig.suptitle(
+            f"Variance ({obs[i]}): initial pure state, "  r"$\alpha / \kappa$" f" = {alpha_over_kappa},\n" f"{NUMBER_OF_TIMEINTERVALS : .1e} time intervals and{NUMBER_OF_TRAJECTORIES : .1e} trajectories."
+        )
+
+    # layout optimization and saving
+    plt.tight_layout()
+    plt.savefig(plotsdir + "var_" + obs[i] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".png", dpi = 300)
+    plt.close(fig)
+
+# === NORMALIZED VARIANCES ===
+
+for i in range(len(obs)):
+    fig, ax = plt.subplots(figsize=(6, 5))
+    # loop over the unravellings
+    for j in range(len(unr)):
+        mean = []
+        var = []
+        mean = np.loadtxt(resultsdir + obs[i] + "_" + unr[j] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".dat", delimiter="\t")
+        var = np.loadtxt(resultsdir + "var_" + obs[i] + "_" + unr[j] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".dat", delimiter="\t")
+        norm_var = []
+        tlist = []
+        for x, y in zip(mean, var):
+            # we append new elements only if the mean is non zero (we cannot divide by zero)
+            if x[1] != 0:
+                tlist.append(x[0])
+                norm_var.append(y[1] / (x[1]**2))
+        ax.plot(tlist, norm_var, color = unr_colors[j], label = r"$" + obs_labels[i] + r"_{" + unr_labels[j] + ", " + str(eta) + r"}$")
+
+    # costumize axes and grids
+    ax.set_xlabel(r"$\kappa t$")
+    ax.set_ylabel(r"$\frac{\sigma^2_" + obs_labels[i] + " (t)}{" +  obs_labels[i] + "^2}$")
+    ax.grid(True, linestyle = ':', alpha = 0.6)
+
+    ax.legend(loc = "upper right", ncol = 2)
+
+    fig.suptitle(
+            f"Normalized variance ({obs[i]}): initial " + instate + " state, "  r"$\alpha / \kappa$" f" = {alpha_over_kappa},\n" f"{NUMBER_OF_TIMEINTERVALS : .1e} time intervals and{NUMBER_OF_TRAJECTORIES : .1e} trajectories."
+        )
+
+    # layout optimization and saving
+    plt.tight_layout()
+    plt.savefig(plotsdir + "norm_var_" + obs[i] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".png", dpi = 300)
+    plt.close(fig)
+
+# === SKEWNESSES ===
+
+for i in range(len(obs)):
+    fig, ax = plt.subplots(figsize=(6, 5))
+    # loop over the unravellings
+    for j in range(len(unr)):
+        data = []
+        data = np.loadtxt(resultsdir + "skw_" + obs[i] + "_" + unr[j] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".dat", delimiter="\t")
+        ax.plot(data[:,0], data[:,1], color = unr_colors[j], label = r"$" + obs_labels[i] + r"_{" + unr_labels[j] + ", " + str(eta) + r"}$")
+
+    # costumize axes and grids
+    ax.set_xlabel(r"$\kappa t$")
+    ax.set_ylabel(r"$\mu^3_" + obs_labels[i] + " (t)$")
+    ax.grid(True, linestyle = ':', alpha = 0.6)
+
+    ax.legend(loc = "upper right", ncol = 2)
+
+    fig.suptitle(
+            f"Skewness ({obs[i]}): initial pure state, "  r"$\alpha / \kappa$" f" = {alpha_over_kappa}\n," f"{NUMBER_OF_TIMEINTERVALS : .1e} time intervals and{NUMBER_OF_TRAJECTORIES : .1e} trajectories."
+        )
+
+    # layout optimization and saving
+    plt.tight_layout()
+    plt.savefig(plotsdir + "skw_" + obs[i] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".png", dpi = 300)
+    plt.close(fig)
+
+# === NORMALIZED SKEWNESSES ===
+
+for i in range(len(obs)):
+    fig, ax = plt.subplots(figsize=(6, 5))
+
+    for j in range(len(unr)):
+        var = []
+        skw = []
+        var = np.loadtxt(resultsdir + "var_" + obs[i] + "_" + unr[j] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".dat", delimiter="\t")
+        skw = np.loadtxt(resultsdir + "skw_" + obs[i] + "_" + unr[j] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".dat", delimiter="\t")
+        norm_skw = []
+        tlist = []
+        for x, y in zip(var, skw):
+            # we append new elements only if the variance is non zero (we cannot divide by zero)
+            if x[1] != 0:
+                tlist.append(x[0])
+                norm_skw.append(y[1] / (x[1]**1.5))
+        ax.plot(tlist, norm_skw, color = unr_colors[j], label = r"$" + obs_labels[i] + r"_{" + unr_labels[j] + ", " + str(eta) + r"}$")
+
+    # costumize axes and grids
+    ax.set_xlabel(r"$\kappa t$")
+    ax.set_ylabel(r"$\frac{\mu^3_" + obs_labels[i] + r" (t)}{(\sigma^2_" + obs_labels[i] + ")^{3/2}}$")
+    ax.grid(True, linestyle = ':', alpha = 0.6)
+
+    ax.legend(loc = "upper right", ncol = 2)
+
+    fig.suptitle(
+            f"Normalized skewness ({obs[i]}): initial " + instate + " state, "  r"$\alpha / \kappa$" f" = {alpha_over_kappa},\n" f"{NUMBER_OF_TIMEINTERVALS : .1e} time intervals and{NUMBER_OF_TRAJECTORIES : .1e} trajectories."
+        )
+
+    # layout optimization and saving
+    plt.tight_layout()
+    plt.savefig(plotsdir + "norm_skw_" + obs[i] + "_" + instate + "_eta" + str(eta) + "_alpha" + str(alpha_over_kappa) + ".png", dpi = 300)
+    plt.close(fig)
