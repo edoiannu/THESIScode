7df01b0726e9e054fe20bdfcb386eabd4832b8d0
Edoardo Iannucci <eiannucci@qtech.fisica.unimi.it>
2026-07-11 10:46:12 +0200

    run da remoto

--- code/submit.sh
+++ code/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=hod90_m_eta1.0_alpha0.4
+#SBATCH --job-name=hod0_p_eta1.0_alpha0.4
 #SBATCH --output=slurm_output/results_%j.log
 #SBATCH --error=slurm_output/errors_%j.log
 #SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=30G
 
-julia --project=. -p $SLURM_NTASKS data_analysis.jl hed
+julia --project=. -p $SLURM_NTASKS data_analysis.jl hod0
