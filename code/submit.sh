7df46662e9179fa910490079eba1159f3f05b342
Edoardo Iannucci <eiannucci@qtech.fisica.unimi.it>
2026-08-11 09:16:04 +0200

    ignore input.dat

--- code/submit.sh
+++ code/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=hed_p_eta1.0_alpha0.4
+#SBATCH --job-name=pd_p_eta0.4_alpha1.0
 #SBATCH --output=slurm_output/results_%j.log
 #SBATCH --error=slurm_output/errors_%j.log
 #SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=30G
 
-julia --project=. -p $SLURM_NTASKS data_analysis.jl hed
+julia --project=. -p $SLURM_NTASKS photodet.jl
