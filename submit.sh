dbc326239aefa734d962693d722e3e2ab1ed332d
Edoardo Iannucci <eiannucci@qtech.fisica.unimi.it>
2026-08-25 12:41:01 +0200

    update

--- submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=ss_pd_eta0.1
-#SBATCH --output=slurm_output/results_%j.log
-#SBATCH --error=slurm_output/errors_%j.log
-#SBATCH --ntasks=10
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=30G
-
-julia --project=. -p $SLURM_NTASKS ss_photodet.jl
